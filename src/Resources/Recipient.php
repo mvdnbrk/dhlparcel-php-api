945bf6bb3bb2383c612e9fa55d96987646f76a85
Mark van den Broek <mvdnbrk@gmail.com>
2019-02-21 16:48:14 +0100

    Adds Recipient resource.

---
+++ src/Resources/Recipient.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Mvdnbrk\DhlParcel\Resources;
+
+class Recipient extends Address
+{
+    /**
+     * @var string
+     */
+    public $company;
+
+    /**
+     * @var string
+     */
+    public $first_name;
+
+    /**
+     * @var string
+     */
+    public $last_name;
+
+    /**
+     * @var string
+     */
+    public $email;
+
+    /**
+     * @var string
+     */
+    public $phone;
+
+    /**
+      * Convert the resource instance to an array.
+      * Removes all attributes with null values.
+      *
+      * @return array
+      */
+    public function toArray()
+    {
+        return collect([
+                'name' => $this->nameToArray(),
+                'address' => $this->addressToArray(),
+            ])
+            ->when(! empty($this->email), function ($collection) {
+                return $collection->put('email', $this->email);
+            })
+            ->when(! empty($this->phone), function ($collection) {
+                return $collection->put('phoneNumber', $this->phone);
+            })
+            ->all();
+    }
+
+    /**
+     * Convert the "address" part of the recipient to an array.
+     *
+     * @return array
+     */
+    private function addressToArray()
+    {
+        return collect(parent::toArray())
+            ->diffKeys([
+                'company' => '',
+                'first_name' => '',
+                'last_name' => '',
+                'email' => '',
+                'phone' => '',
+            ])
+            ->when(! empty($this->company), function ($collection) {
+                return $collection->put('isBusiness', true);
+            })
+            ->all();
+    }
+
+    /**
+     * Convert the "name" part of the recipient to an array.
+     *
+     * @return array
+     */
+    private function nameToArray()
+    {
+        return collect([
+                'firstName' => $this->first_name,
+                'lastName' => $this->last_name,
+                'companyName' => $this->company,
+            ])
+            ->reject(function ($value) {
+                return $value === null;
+            })
+            ->all();
+    }
+}
---
+++ tests/Unit/Resources/RecipientTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Mvdnbrk\DhlParcel\Tests\Unit\Resources;
+
+use Mvdnbrk\DhlParcel\Tests\TestCase;
+use Mvdnbrk\DhlParcel\Resources\Recipient;
+
+class RecipientTest extends TestCase
+{
+    private function validParams($overrides = [])
+    {
+        return array_merge([
+            'company' => 'Test Company B.V.',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john@example.com',
+            'phone' => '0101111111',
+        ], $overrides);
+    }
+
+    /** @test */
+    public function creating_a_valid_recipient_resource()
+    {
+        $recipient = new Recipient([
+            'company' => 'Test Company B.V.',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john@example.com',
+            'phone' => '0101111111',
+        ]);
+
+        $this->assertEquals('Test Company B.V.', $recipient->company);
+        $this->assertEquals('John', $recipient->first_name);
+        $this->assertEquals('Doe', $recipient->last_name);
+        $this->assertEquals('john@example.com', $recipient->email);
+        $this->assertEquals('0101111111', $recipient->phone);
+    }
+
+    /** @test */
+    public function to_array()
+    {
+        $attributes = [
+            'company' => 'Test Company B.V.',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john@example.com',
+            'phone' => '0101111111',
+            'street' => 'Poststraat',
+            'number' => '1',
+            'postal_code' => '1234AA',
+            'city' => 'Amsterdam',
+            'region' => 'Noord-Holland',
+            'cc' => 'NL',
+        ];
+
+        $recipient = new Recipient($attributes);
+
+        $array = $recipient->toArray();
+
+        $this->assertInternalType('array', $array);
+
+        $this->assertArrayHasKey('name', $array);
+        $this->assertInternalType('array', $array['name']);
+        $this->assertEquals('John', $array['name']['firstName']);
+        $this->assertEquals('Doe', $array['name']['lastName']);
+        $this->assertEquals('Test Company B.V.', $array['name']['companyName']);
+
+        $this->assertArrayHasKey('address', $array);
+        $this->assertInternalType('array', $array['address']);
+        $this->assertEquals('Poststraat', $array['address']['street']);
+        $this->assertEquals('1', $array['address']['number']);
+        $this->assertEquals('1234AA', $array['address']['postalCode']);
+        $this->assertEquals('Amsterdam', $array['address']['city']);
+        $this->assertEquals('NL', $array['address']['countryCode']);
+
+        $this->assertTrue($array['address']['isBusiness']);
+
+        $this->assertArrayHasKey('email', $array);
+        $this->assertEquals('john@example.com', $array['email']);
+        $this->assertArrayHasKey('phoneNumber', $array);
+        $this->assertEquals('0101111111', $array['phoneNumber']);
+        $this->assertArrayNotHasKey('phone', $array);
+
+        $recipient->email = null;
+        $recipient->phone = null;
+        $recipient->company = null;
+        $array = $recipient->toArray();
+        $this->assertArrayNotHasKey('email', $array);
+        $this->assertArrayNotHasKey('phone', $array);
+        $this->assertArrayNotHasKey('phoneNumber', $array);
+        $this->assertArrayNotHasKey('companyName', $array['name']);
+        $this->assertFalse($array['address']['isBusiness']);
+    }
+}
