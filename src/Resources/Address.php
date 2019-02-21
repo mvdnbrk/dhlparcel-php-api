209ebb9e32e14fdc7c43e3ee4ab76843750adade
Mark van den Broek <mvdnbrk@gmail.com>
2019-02-21 16:48:01 +0100

    Adds Address resource.

---
+++ src/Resources/Address.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Mvdnbrk\DhlParcel\Resources;
+
+use Mvdnbrk\DhlParcel\Support\Str;
+
+class Address extends BaseResource
+{
+    /**
+     * @var string
+     */
+    public $street;
+
+    /**
+     * @var int|string
+     */
+    public $number;
+
+    /**
+     * @var string
+     */
+    public $number_suffix;
+
+    /**
+     * @var string
+     */
+    public $postal_code;
+
+    /**
+     * @var string
+     */
+    public $city;
+
+    /**
+     * @var string
+     */
+    public $region;
+
+    /**
+     * ISO3166-1 country code.
+     * https://en.wikipedia.org/wiki/ISO_3166-1
+     *
+     * @var string
+     */
+    public $cc;
+
+    /**
+     * Set country code.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setCcAttribute($value)
+    {
+        $this->cc = Str::upper($value);
+    }
+
+    /**
+     * Sets the country code. Alias for cc.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setCountryCodeAttribute($value)
+    {
+        $this->setCcAttribute($value);
+    }
+
+    /**
+     * Set the postal code.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setPostalCodeAttribute($value)
+    {
+        $this->postal_code = Str::upper($value);
+    }
+
+    /**
+     * Sets the zipcode. Alias for postal_code.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setZipcodeAttribute($value)
+    {
+        $this->setPostalCodeAttribute($value);
+    }
+
+    /**
+      * Convert the resource instance to an array.
+      * Removes all attributes with null values.
+      *
+      * @return array
+      */
+    public function toArray()
+    {
+        return collect(parent::toArray())
+            ->transform(function ($value, $key) {
+                if ($key == 'number') {
+                    return (string) $value;
+                }
+
+                return $value;
+            })
+            ->put('isBusiness', false)
+            ->when(! empty($this->number_suffix), function ($collection) {
+                return $collection
+                    ->put('addition', $this->number_suffix)
+                    ->forget('number_suffix');
+            })
+            ->put('postalCode', $this->postal_code)
+            ->put('countryCode', $this->cc)
+            ->forget('postal_code')
+            ->forget('cc')
+            ->all();
+    }
+}
---
+++ tests/Unit/Resources/AddressTest.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Mvdnbrk\DhlParcel\Tests\Unit\Resources;
+
+use Mvdnbrk\DhlParcel\Tests\TestCase;
+use Mvdnbrk\DhlParcel\Resources\Address;
+
+class AddressTest extends TestCase
+{
+    private function validParams($overrides = [])
+    {
+        return array_merge([
+            'street' => 'Poststraat',
+            'number' => '1',
+            'number_suffix' => 'A',
+            'postal_code' => '1234AA',
+            'city' => 'Amsterdam',
+            'region' => 'Noord-Holland',
+            'cc' => 'NL',
+        ], $overrides);
+    }
+
+    /** @test */
+    public function creating_a_valid_address_resource()
+    {
+        $address = new Address([
+            'street' => 'Poststraat',
+            'number' => '1',
+            'number_suffix' => 'A',
+            'postal_code' => '1234AA',
+            'city' => 'Amsterdam',
+            'region' => 'Noord-Holland',
+            'cc' => 'NL',
+        ]);
+
+        $this->assertEquals('Poststraat', $address->street);
+        $this->assertEquals('1', $address->number);
+        $this->assertEquals('A', $address->number_suffix);
+        $this->assertEquals('1234AA', $address->postal_code);
+        $this->assertEquals('Amsterdam', $address->city);
+        $this->assertEquals('Noord-Holland', $address->region);
+        $this->assertEquals('NL', $address->cc);
+    }
+
+    /** @test */
+    public function country_code_may_be_used_as_an_alias_to_cc()
+    {
+        $address = new Address([
+            'country_code' => 'NL'
+        ]);
+
+        $this->assertEquals('NL', $address->cc);
+    }
+
+    /** @test */
+    public function zipcode_may_be_used_as_an_alias_to_postal_code()
+    {
+        $address = new Address([
+            'zipcode' => '9999ZZ'
+        ]);
+
+        $this->assertEquals('9999ZZ', $address->postal_code);
+    }
+
+    /** @test */
+    public function lower_case_country_code_is_converted_to_uppercase()
+    {
+        $address = new Address($this->validParams([
+            'cc' => 'nl'
+        ]));
+
+        $this->assertEquals('NL', $address->cc);
+    }
+
+    /** @test */
+    public function lower_case_postal_code_is_converted_to_uppercase()
+    {
+        $address = new Address($this->validParams([
+            'postal_code' => '1234aa'
+        ]));
+
+        $this->assertEquals('1234AA', $address->postal_code);
+    }
+
+    /** @test */
+    public function number_should_be_casted_to_a_string()
+    {
+        $address = new Address($this->validParams([
+            'number' => 999
+        ]));
+
+        $array = $address->toArray();
+
+        $this->assertInternalType('string', $array['number']);
+        $this->assertEquals('999', $array['number']);
+    }
+
+    /** @test */
+    public function to_array()
+    {
+        $attributes = [
+            'street' => 'Poststraat',
+            'number' => '1',
+            'number_suffix' => 'A',
+            'postal_code' => '1234AA',
+            'city' => 'Amsterdam',
+            'region' => 'Noord-Holland',
+            'cc' => 'NL',
+        ];
+
+        $address = new Address($attributes);
+
+        $array = $address->toArray();
+
+        $this->assertInternalType('array', $array);
+        $this->assertFalse($array['isBusiness']);
+        $this->assertEquals('Poststraat', $array['street']);
+        $this->assertEquals('1', $array['number']);
+        $this->assertEquals('A', $array['addition']);
+        $this->assertEquals('1234AA', $array['postalCode']);
+        $this->assertEquals('NL', $array['countryCode']);
+        $this->assertArrayNotHasKey('number_suffix', $array);
+        $this->assertArrayNotHasKey('postal_code', $array);
+        $this->assertArrayNotHasKey('cc', $array);
+
+        $address->number_suffix = null;
+        $array = $address->toArray();
+        $this->assertArrayNotHasKey('addition', $array);
+        $this->assertArrayNotHasKey('number_suffix', $array);
+    }
+}
