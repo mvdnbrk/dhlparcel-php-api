71d14cdc4335669a0ca2df25320d9aa7efb92648
Mark van den Broek <mvdnbrk@gmail.com>
2019-02-22 18:51:35 +0100

    Adds ShipmentTest.

---
+++ tests/Unit/Resources/ShipmentTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Mvdnbrk\DhlParcel\Tests\Unit\Resources;
+
+use Mvdnbrk\DhlParcel\Tests\TestCase;
+use Mvdnbrk\DhlParcel\Resources\Shipment;
+
+class ShipmentTest extends TestCase
+{
+    /** @test */
+    public function create_a_new_shipment()
+    {
+        $shipment = new Shipment([
+            'id' => '123456',
+            'barcode' => 'JVGL11112222333344445555',
+            'label_id' => '12345678-aaaa-bbbb-cccc-123456789123'
+        ]);
+
+        $this->assertEquals('123456', $shipment->id);
+        $this->assertEquals('JVGL11112222333344445555', $shipment->barcode);
+        $this->assertEquals('12345678-aaaa-bbbb-cccc-123456789123', $shipment->label_id);
+    }
+}
