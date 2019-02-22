26ba2e24e1e5aead8fd77aa07f48f8693c8bf9ff
Mark van den Broek <mvdnbrk@gmail.com>
2019-02-22 17:56:04 +0100

    Updates Client endpoint.

--- src/Client.php
+++ src/Client.php
@@ -2,9 +2,14 @@
 
 namespace Mvdnbrk\DhlParcel;
 
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\RequestOptions;
 use Composer\CaBundle\CaBundle;
 use GuzzleHttp\Client as HttpClient;
+use Mvdnbrk\DhlParcel\Endpoints\Labels;
+use Mvdnbrk\DhlParcel\Endpoints\Shipments;
+use Mvdnbrk\DhlParcel\Endpoints\Authentication;
+use Mvdnbrk\DhlParcel\Exceptions\DhlParcelException;
 
 class Client
 {
@@ -34,6 +39,21 @@ class Client
     protected $httpClient;
 
     /**
+     * @var \Mvdnbrk\DhlParcel\Endpoints\Authentication
+     */
+    public $authentication;
+
+    /**
+     * @var \Mvdnbrk\DhlParcel\Endpoints\Labels
+     */
+    public $labels;
+
+    /**
+     * @var \Mvdnbrk\DhlParcel\Endpoints\Shipments
+     */
+    public $shipments;
+
+    /**
      * Create a new Client instance.
      *
      * @return void
@@ -43,6 +63,57 @@ class Client
         $this->httpClient = new HttpClient([
             RequestOptions::VERIFY => CaBundle::getBundledCaBundlePath()
         ]);
+
+        $this->initializeEndpoints();
+    }
+    /**
+     * Initialize the API endpoints used by this client.
+     *
+     * @return void
+     */
+    public function initializeEndpoints()
+    {
+        $this->authentication = new Authentication($this);
+        $this->labels = new Labels($this);
+        $this->shipments = new Shipments($this);
+    }
+
+    /**
+     * Performs a HTTP call to the API endpoint
+     *
+     * @param  string  $httpMethod          The method to make the API call. GET/POST etc,
+     * @param  string  $apiMethod           The API method to call at the endpoint.
+     * @param  string|null  $httpBody       The body to be send with te request.
+     * @param  array  $requestHeaders       Request headers to be send with the request.
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \Mvdnbrk\DhlParcel\Exceptions\DhlParcelException
+     */
+    public function performHttpCall($httpMethod, $apiMethod, $httpBody = null, $requestHeaders = [])
+    {
+        $url = $this->apiEndpoint . '/' . $apiMethod;
+
+        $headers = collect([
+                'Accept' => 'application/json',
+            ])
+            ->merge($requestHeaders)
+            ->when($httpBody !== null, function ($headers) {
+                return $headers->put('Content-Type', 'application/json');
+            })
+            ->all();
+
+        $request = new Request($httpMethod, $url, $headers, $httpBody);
+
+        try {
+            $response = $this->httpClient->send($request, ['http_errors' => false]);
+        } catch (GuzzleException $e) {
+            throw new DhlParcelException($e->getMessage(), $e->getCode());
+        }
+
+        if (! $response) {
+            throw new DhlParcelException('No API response received.');
+        }
+
+        return $response;
     }
 
     /**
@@ -69,5 +140,27 @@ class Client
         $this->userId = trim($value);
 
         return $this;
+    }
+
+    /**
+     * Retrieve the credentials.
+     *
+     * @return array
+     * @throws \Mvdnbrk\DhlParcel\Exceptions\DhlParcelException
+     */
+    public function credentials()
+    {
+        if (empty($this->userId)) {
+            throw new DhlParcelException('You have not set a user id. Please use setUserId() to set the user id.');
+        }
+
+        if (empty($this->apiKey)) {
+            throw new DhlParcelException('You have not set an API key. Please use setApiKey() to set the API key.');
+        }
+
+        return [
+            'userId' => $this->userId,
+            'key' => $this->apiKey,
+        ];
     }
 }
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,12 +2,61 @@
 namespace Mvdnbrk\DhlParcel\Tests;
 
 use Mvdnbrk\DhlParcel\Client;
+use Mvdnbrk\DhlParcel\Endpoints\Labels;
+use Mvdnbrk\DhlParcel\Endpoints\Shipments;
+use Mvdnbrk\DhlParcel\Endpoints\Authentication;
+use Mvdnbrk\DhlParcel\Exceptions\DhlParcelException;
 
 class ClientTest extends TestCase
 {
     /** @test */
-    public function true_is_true()
+    public function it_has_an_authentication_endpoint()
     {
-        $this->assertTrue(true);
+        $this->assertInstanceOf(Authentication::class, $this->client->authentication);
+    }
+
+    /** @test */
+    public function it_has_a_shipments_endpoint()
+    {
+        $this->assertInstanceOf(Shipments::class, $this->client->shipments);
+    }
+
+    /** @test */
+    public function it_has_a_labels()
+    {
+        $this->assertInstanceOf(Labels::class, $this->client->labels);
+    }
+
+    /** @test */
+    public function it_can_set_the_credentials()
+    {
+        $client = (new Client)->setApiKey('test-key')->setUserId('1234');
+
+        $this->assertInternalType('array', $client->credentials());
+        $this->assertEquals(['key' => 'test-key', 'userId' => '1234'], $client->credentials());
+    }
+
+    /** @test */
+    public function setting_an_empty_api_key_throws_an_exception()
+    {
+        $this->expectException(DhlParcelException::class);
+        $this->expectExceptionMessage('You have not set an API key. Please use setApiKey() to set the API key.');
+
+        $this->client->setApiKey(null);
+        $this->client->setUserId('1234');
+
+        $this->client->credentials();
+    }
+
+    /** @test */
+    public function setting_an_empty_user_id_throws_an_exception()
+    {
+        $this->expectException(DhlParcelException::class);
+        $this->expectExceptionMessage('You have not set a user id. Please use setUserId() to set the user id.');
+
+        $this->client->setApiKey('test-key');
+        $this->client->setUserId(null);
+
+        $this->client->credentials();
     }
 }
