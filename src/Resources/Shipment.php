91b2cb73af77a097b596f950c48aa03705890f78
Mark van den Broek <mvdnbrk@gmail.com>
2019-02-23 19:57:45 +0100

    Convert a shipment to an array.

--- src/Resources/Shipment.php
+++ src/Resources/Shipment.php
@@ -18,4 +18,18 @@ class Shipment extends Parcel
      * @var string
      */
     public $label_id;
+
+    /**
+      * Convert the parcel resource to an array.
+      *
+      * @return array
+      */
+    public function toArray()
+    {
+        return collect(parent::toArray())->merge([
+            'id' => $this->id,
+            'barcode' => $this->barcode,
+            'label_id' => $this->label_id,
+        ])->all();
+    }
 }
