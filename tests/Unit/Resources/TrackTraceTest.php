d9869884a9dd30c05dc504affb76b24cbcde4239
Mark van den Broek <mvdnbrk@gmail.com>
2019-03-15 15:07:23 +0100

    Adds TrackTrace resource.

---
+++ tests/Unit/Resources/TrackTraceTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Mvdnbrk\DhlParcel\Tests\Unit\Resources;
+
+use Mvdnbrk\DhlParcel\Tests\TestCase;
+use Mvdnbrk\DhlParcel\Resources\TrackTrace;
+
+class ShipmentTest extends TestCase
+{
+    /** @test */
+    public function creating_a_new_track_trace_resource()
+    {
+        $tracktrace = new TrackTrace([
+            'barcode' => 'JVGL11112222333344445555',
+        ]);
+
+        $this->assertEquals('JVGL11112222333344445555', $tracktrace->barcode);
+        $this->assertFalse($tracktrace->isDelivered);
+    }
+
+    /** @test */
+    public function it_can_determine_if_the_parcel_is_delivered()
+    {
+        $tracktrace = new TrackTrace();
+
+        $this->assertFalse($tracktrace->isDelivered);
+
+        $tracktrace = new TrackTrace([
+            'barcode' => 'JVGL11112222333344445555',
+            'deliveredAt' => '2019-01-01T12:00:00.000+00:00',
+        ]);
+
+        $this->assertTrue($tracktrace->isDelivered);
+    }
+}
