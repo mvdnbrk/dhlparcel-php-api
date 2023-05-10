4e8281bdcafdca067c92076d374eb8a529bed819
Willem Turkstra <willem.turkstra@wics.nl>
2023-05-10 11:46:47 +0200

    Added phpdoc

--- src/Resources/Option.php
+++ src/Resources/Option.php
@@ -4,9 +4,18 @@ namespace Mvdnbrk\DhlParcel\Resources;
 
 class Option extends BaseResource
 {
+    /** @var string */
     public $key;
+
+    /** @var string */
     public $description;
+
+    /** @var int */
     public $rank;
+
+    /** @var int */
     public $code;
+
+    /** @var string */
     public $optionType;
 }
